280ac9852a71e2998efdb8777d009fb8760dadd5
ahmednoorzakaria <ahmednoor.zakaria@student.moringaschool.com>
2023-06-09 09:46:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    let messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      messages.push(message);
+    }
+  
+    return messages;
+  }
+const names = ["Ada", "Brendan", "Ali"];
+const eventName = "birthday";
+
+const thankYouMessages = writeCards(names, eventName);
+console.log(thankYouMessages);
+
+
+function countDown(count) {
+    while (count >= 0) {
+      console.log(count);
+      count--;
+    }
+  }
+  
+  let count = 10;
+  countDown(count);
